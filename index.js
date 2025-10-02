2870898c2ce782819d037cd8d64ba22ee81c2706
SerenKoca <r0937023@student.thomasmore.be>
2025-10-02 14:57:31 +0200

    added cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const logger = require('./middleware/logger');
 const app = express();
+const cors = require('cors');
 const port = 3000;
 const messagesRouter = require('./routes/v1/messages');
 
-
+app.use(cors());
 app.use("/api/v1/messages/", messagesRouter);//word alleen op /api/v1/messages gebruikt, maakt niet echt uit wat er staat kan ook Seren staan ofzo
 app.use(express.json());//word op alles gebruikt. gebruikt om tussen backend en frontend te praten in Json en gaat json zo activeren. op elke route moet json werken
 
