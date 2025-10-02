657c02e511ca4eb84de54cfe13628ce5daf1ef75
SerenKoca <r0937023@student.thomasmore.be>
2025-10-02 13:52:49 +0200

    Test github

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const logger = require('./middleware/logger');
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 let messages = [
     "hello",
