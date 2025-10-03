7d9437cc9d6e7988bee0256a977d6d79fa95b421
SerenKoca <r0937023@student.thomasmore.be>
2025-10-03 21:05:51 +0200

    fix: reorder middleware usage for clarity in index.js

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const port = 3000;
 const messagesRouter = require('./routes/v1/messages');
 
 app.use(cors());//cors niet kunnen niet
-app.use("/api/v1/messages/", messagesRouter);//word alleen op /api/v1/messages gebruikt, maakt niet echt uit wat er staat kan ook Seren staan ofzo
 app.use(express.json());//word op alles gebruikt. gebruikt om tussen backend en frontend te praten in Json en gaat json zo activeren. op elke route moet json werken
+app.use("/api/v1/messages/", messagesRouter);//word alleen op /api/v1/messages gebruikt, maakt niet echt uit wat er staat kan ook Seren staan ofzo
 
 
 app.listen(port, () => {
